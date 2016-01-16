cfb6d76bd71599b1978ce604f31fc55c9bd77037
BlockchainPhil <blockchain.phil@gmail.com>
2016-01-16 19:21:34 +0000

    finished server.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-x
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.use(express.static('web'));
+
+app.listen(port, function () {
+  console.log('Server listening on port ' + port + '! Press Ctrl + C to exit');
+})
